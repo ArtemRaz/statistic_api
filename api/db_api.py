eb37b915674fd21ee3133a873193f0635f393c41
ArtemRaz <artemraz@mail.ru>
2024-04-09 13:15:46 +0300

    Add new table with new format of data

--- api/db_api.py
+++ api/db_api.py
@@ -1,13 +1,14 @@
 from flask import request
-from models import GameMatches
+from models.game_matches_new import GameMatchesNew
 from utils import try_parse_int
+from datetime import datetime
 from app import app
 
 
 @app.route("/add_statistic", methods=["POST"])
 def add_statistic():
-    datestart = request.form.get('datestart')
-    durationmatch = request.form.get('durationmatch')
+    datestart = datetime.strptime(request.form.get('datestart'), "%Y.%m.%d").date() if request.form.get('datestart') else None
+    durationmatch = datetime.strptime(request.form.get('durationmatch'), "%M:%S").time() if request.form.get('durationmatch') else None
     game = request.form.get('game')
     idlocation = try_parse_int(request.form.get('idlocation'))
     idsession = try_parse_int(request.form.get('idsession'))
@@ -15,20 +16,20 @@ def add_statistic():
     playedmaps = request.form.get('playedmaps')
     players = try_parse_int(request.form.get('players'))
     submodes = request.form.get('submodes')
-    timeend = request.form.get('timeend')
-    timestart = request.form.get('timestart')
+    timeend = datetime.strptime(f"{request.form.get('datestart')} {request.form.get('timeend')}", "%Y.%m.%d %H:%M:%S") if request.form.get('timeend') else None
+    timestart = datetime.strptime(f"{request.form.get('datestart')} {request.form.get('timestart')}", "%Y.%m.%d %H:%M:%S") if request.form.get('timestart') else None
 
-    id = GameMatches.create(datestart=datestart,
-                            durationmatch=durationmatch,
-                            game=game,
-                            idlocation=idlocation,
-                            idsession=idsession,
-                            modes=modes,
-                            playedmaps=playedmaps,
-                            players=players,
-                            submodes=submodes,
-                            timeend=timeend,
-                            timestart=timestart)
+    id = GameMatchesNew.create(datestart=datestart,
+                               durationmatch=durationmatch,
+                               game=game,
+                               idlocation=idlocation,
+                               idsession=idsession,
+                               modes=modes,
+                               playedmaps=playedmaps,
+                               players=players,
+                               submodes=submodes,
+                               timeend=timeend,
+                               timestart=timestart)
 
     return {'id': id.id}, 200
 
