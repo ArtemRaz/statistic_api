bada16963c69ca052d2e8e70395f5429bb2e515a
ArtemRaz <artemraz@mail.ru>
2024-04-12 11:56:04 +0300

    Replace serial id to UUID at Skin and Weapon tables

--- models/skin.py
+++ models/skin.py
@@ -1,7 +1,10 @@
+import uuid
+
 from db import *
 
 
 class Skin(BaseModel):
+    id = UUIDField(primary_key=True, default=uuid.uuid4, constraints=[SQL('DEFAULT gen_random_uuid()')])
     name = CharField(null=True)
     image_url = CharField(null=True)
 
--- models/weapon.py
+++ models/weapon.py
@@ -1,7 +1,9 @@
+import uuid
+
 from db import *
 
-
 class Weapon(BaseModel):
+    id = UUIDField(primary_key=True, default=uuid.uuid4, constraints=[SQL('DEFAULT gen_random_uuid()')])
     name = CharField(null=True)
     image_url = CharField(null=True)
     bullets = IntegerField(null=True)
